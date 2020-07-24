d4e945de78c69a23e22cec5fd3cee741a6f85622
Sumtudou <1163015195@qq.com>
2020-07-24 16:48:00 +0800

    [park car][Jay] feature:pass should_return_ticket_when_parkCar_given_1car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,12 +1,18 @@
 package com.oocl.cultivation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParkingBoy {
-
-    public Car getCar(Ticket ticket) {
-        return null;
-    }
-
+//    Set<Car> parkedCarSet;
+//
+//    public ParkingBoy(){
+//        parkedCarSet = new HashSet<>();
+//    }
+//    public Car getCar(Ticket ticket) {
+//        return null;
+//    }
     public Ticket parkCar(Car car) {
-        return null;
+        return new Ticket();
     }
 }
