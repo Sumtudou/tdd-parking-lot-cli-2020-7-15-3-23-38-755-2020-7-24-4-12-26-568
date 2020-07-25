29821947caefae6ebaea2524219a78eb0d4e62f4
Sumtudou <1163015195@qq.com>
2020-07-25 14:30:43 +0800

    [park car][Jay] test:should_return_null_when_parkCar_given_parking_is_full

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -6,6 +6,9 @@ import com.oocl.cultivation.Ticket;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -77,6 +80,39 @@ class ParkingBoyTest {
 
         //then
         assertEquals(null, gotCar);
+    }
+
+    @Test
+    void should_return_null_when_parkCar_given_parking_is_full() {
+        //given
+        Car car1 = new Car("C001");
+        Car car2 = new Car("C002");
+        Car car3 = new Car("C003");
+        Car car4 = new Car("C004");
+        Car car5 = new Car("C005");
+        Car car6 = new Car("C006");
+        Car car7 = new Car("C007");
+        Car car8 = new Car("C008");
+        Car car9 = new Car("C009");
+        Car car10 = new Car("C010");
+        Car car11 = new Car("C011");
+
+        ParkingBoy parkingBoy = new ParkingBoy();
+        //when
+        Ticket ticket1 = parkingBoy.parkCar(car1,"T001");
+        Ticket ticket2 = parkingBoy.parkCar(car2,"T002");
+        Ticket ticket3 = parkingBoy.parkCar(car3,"T003");
+        Ticket ticket4 = parkingBoy.parkCar(car4,"T004");
+        Ticket ticket5 = parkingBoy.parkCar(car5,"T005");
+        Ticket ticket6 = parkingBoy.parkCar(car6,"T006");
+        Ticket ticket7 = parkingBoy.parkCar(car7,"T007");
+        Ticket ticket8 = parkingBoy.parkCar(car8,"T008");
+        Ticket ticket9 = parkingBoy.parkCar(car9,"T009");
+        Ticket ticket10 = parkingBoy.parkCar(car10,"T010");
+        Ticket ticket11 = parkingBoy.parkCar(car11,"T011");
+
+        //then
+        assertEquals(null, ticket11);
     }
 
 
