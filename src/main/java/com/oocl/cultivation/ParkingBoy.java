b70ca5c117275c29f5abc9b1f54906b9bdcf0018
Sumtudou <1163015195@qq.com>
2020-07-25 15:43:35 +0800

    [park car02][Jay] pass:should_return_give_me_ticket_when_getCar_given_null

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -13,16 +13,15 @@ public class ParkingBoy {
     }
 
 
-    public ParkingLot buildParkingLot(Integer maxSize){
+    public ParkingLot buildParkingLot(Integer maxSize) {
         this.parkingLot = new ParkingLot(maxSize);
         return parkingLot;
     }
 
 
-
     public Ticket parkCar(Car car, String token) {
         Set<Car> parkingLotSet = parkingLot.getParkingLotSet();
-        if(car == null || token == null)
+        if (car == null || token == null)
             return null;
         if (parkingLotSet.size() < parkingLot.getMaxSize() && !parkingLotSet.contains(car)) {
             parkingLotSet.add(car);
@@ -34,18 +33,23 @@ public class ParkingBoy {
 
     public Car getOutCar(Ticket ticket) {
         Car errorCar = new Car();
-        if (ticket == null || ticket.getUsed() || ticket.getCar() == null){
-            errorCar.setErrorMsg("Unrecognized parking ticket.");
-            return  errorCar;
+        if (ticket == null) {
+            errorCar.setErrorMsg("Please provide your parking ticket.");
+            return errorCar;
         }
+        if (ticket.getUsed() || ticket.getCar() == null) {
+            errorCar.setErrorMsg("Unrecognized parking ticket.");
+            return errorCar;
+        }
+
         Car car = ticket.getCar();
         Set<Car> parkingLotSet = parkingLot.getParkingLotSet();
-        if(parkingLotSet.contains(car)){  //this car is in parkingLot
+        if (parkingLotSet.contains(car)) {  //this car is in parkingLot
             parkingLotSet.remove(car);
             parkingLot.setParkingLotSet(parkingLotSet);
             ticket.setUsed(true);
             return car;
-        }else{
+        } else {
             errorCar.setErrorMsg("This car has been stolen");
             return errorCar;
         }
